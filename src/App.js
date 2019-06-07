a8fe89dfd45ed1ac4f020ea24892eab2f8e4de72
Diego Miguel <digznav@gmail.com>
2019-06-07 11:49:11 -0500

    Source files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+const Pet = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed)
+  ]);
+}
+
+const App = () => {
+  return React.createElement(
+    'div',
+    {id: 'something important'},
+    [
+      React.createElement('h1', {}, 'Adopt me !'),
+      React.createElement(Pet, {
+        name: 'Luna',
+        animal: 'dog',
+        breed: 'Mixed'
+      }),
+      React.createElement(Pet, {
+        name: 'Chewie',
+        animal: 'dog',
+        breed: 'Chihuahua'
+      }),
+      React.createElement(Pet, {
+        name: 'Doink',
+        animal: 'cat',
+        breed: 'Mixed'
+      })
+    ]
+  );
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
