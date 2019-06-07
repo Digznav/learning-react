bccadf092c1631580e813830d981b244bcd6718d
Diego Miguel <digznav@gmail.com>
2019-06-07 12:42:37 -0500

    Split Pet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Pet from './Pet';
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ]);
-};
-
-const App = () => {
+function App() {
   return React.createElement('div', { id: 'something important' }, [
     React.createElement('h1', {}, 'Adopt me !'),
     React.createElement(Pet, {
