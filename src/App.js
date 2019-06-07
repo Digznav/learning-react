151d21f23f416320f3e3f2c84ab28991471b5319
Diego Miguel <digznav@gmail.com>
2019-06-07 14:19:37 -0500

    Init search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 function App() {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Mixed" />
-      <Pet name="Chewie" animal="Dog" breed="Chihuahua" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 }
---
+++ src/SearchParams.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SearchParams() {
+  const location = 'Seattle, WA';
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input type="text" id="location" value={location} placeholder="Location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchParams;
