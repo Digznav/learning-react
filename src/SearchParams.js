c8f3eb1314fb1e9d27e37365e5dc6a3c1743d18f
Diego Miguel <digznav@gmail.com>
2019-06-07 15:37:33 -0500

    Extending form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 function SearchParams() {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('dog');
+  const [breed, setBreed] = useState('');
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -13,10 +17,41 @@ function SearchParams() {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={event => {
-              setLocation(event.target.value);
-            }}
+            onChange={event => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={event => setBreed(event.target.value)}
+            onBlur={event => setBreed(event.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map(family => (
+              <option key={family} value={family}>
+                {family}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
