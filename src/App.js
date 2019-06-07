3dbfd3ba2782215a73759ec0bbdbf6199dc3e7a7
Diego Miguel <digznav@gmail.com>
2019-06-07 12:22:40 -0500

    Eslint

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, name),
     React.createElement('h2', {}, animal),
     React.createElement('h2', {}, breed)
   ]);
-}
+};
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {id: 'something important'},
-    [
-      React.createElement('h1', {}, 'Adopt me !'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'dog',
-        breed: 'Mixed'
-      }),
-      React.createElement(Pet, {
-        name: 'Chewie',
-        animal: 'dog',
-        breed: 'Chihuahua'
-      }),
-      React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'cat',
-        breed: 'Mixed'
-      })
-    ]
-  );
-}
+  return React.createElement('div', { id: 'something important' }, [
+    React.createElement('h1', {}, 'Adopt me !'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'dog',
+      breed: 'Mixed'
+    }),
+    React.createElement(Pet, {
+      name: 'Chewie',
+      animal: 'dog',
+      breed: 'Chihuahua'
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'cat',
+      breed: 'Mixed'
+    })
+  ]);
+};
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
